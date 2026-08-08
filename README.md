b5dda7acb62804e68c6e327b95758fe055992942
Swarit <swaritsharma2010@gmail.com>
2026-08-08 10:21:21 +0530

    Created first terminal based problem solver

---
+++ README.md
@@ -0,0 +1,3 @@
+# A working amount in rupees distinguisher
+<br> 
+A Program that will divide your amount among corresponding 500,200,100,50,20,10,5,2,1 rupee notes. I will help you in dividing your money in different denominations most helful for small businesses.
---
+++ src/App/Code.java
@@ -0,0 +1,20 @@
+package App;
+import java.util.*;
+public class Code {
+    static Scanner sc = new Scanner(System.in);
+    public static int input(){
+        return sc.nextInt();
+    }
+    static void denominations(){
+        int[] denom = {500,200,100,50,20,10,5,2,1};
+        System.out.println("Enter the amount :");
+        int amt = input();
+        for(int i = 0;i < denom.length;i++){
+            System.out.println("The number of " + denom[i] + " notes are " + amt/denom[i]);
+            amt%=denom[i];
+        }
+    }
+    static void main() {
+        denominations();
+    }
+}
